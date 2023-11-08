47faafa125f6450b406bbfa22023025bd4ac9e08
Benjababe <bengohzy@gmail.com>
2023-11-08 11:53:49 +0800

    add door cam startup message

--- esp32-door-cam/esp32-web-client.cpp
+++ esp32-door-cam/esp32-web-client.cpp
@@ -40,6 +40,10 @@ void WebClient::connectAWS() {
     this->mqttClient->onMessage(onInvalidEntry);
     this->mqttClient->subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
     Serial.println("\nAWS IOT Core connected!");
+
+    for (String chatId : acceptedIds) {
+      bot.sendMessage(chatId, "[Smart Lock System] Camera up and running");
+    }
   }
 }
 
@@ -70,76 +74,73 @@ void sendPhotoTelegram(String chatId) {
   String getAll = "";
   String getBody = "";
 
-  camera_fb_t * fb = NULL;
-  fb = esp_camera_fb_get();  
-  if(!fb) {
+  camera_fb_t* fb = NULL;
+  fb = esp_camera_fb_get();
+  if (!fb) {
     Serial.println("Camera capture failed");
     delay(1000);
     ESP.restart();
     return;
-  }  
+  }
   Serial.println("Connecting to " + myDomain);
   if (clientTele.connect(myDomain.c_str(), 443)) {
     Serial.println("Connection successful");
-    
+
     String head = "--cam\r\nContent-Disposition: form-data; name=\"chat_id\"; \r\n\r\n" + chatId + "\r\n";
-    head += "--cam\r\nContent-Disposition: form-data; name=\"caption\"\r\n\r\nInvalid entry attempted on ESP32 lock!\r\n";
+    head += "--cam\r\nContent-Disposition: form-data; name=\"caption\"\r\n\r\n[Smart Lock System] Invalid entry attempted on ESP32 lock!\r\n";
     head += "--cam\r\nContent-Disposition: form-data; name=\"photo\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
     String tail = "\r\n--cam--\r\n";
 
     uint16_t imageLen = fb->len;
     uint16_t extraLen = head.length() + tail.length();
     uint16_t totalLen = imageLen + extraLen;
-  
-    clientTele.println("POST /bot"+ String(SECRET_TELEGRAM_BOT_TOKEN) +"/sendPhoto HTTP/1.1");
+
+    clientTele.println("POST /bot" + String(SECRET_TELEGRAM_BOT_TOKEN) + "/sendPhoto HTTP/1.1");
     clientTele.println("Host: " + myDomain);
     clientTele.println("Content-Length: " + String(totalLen));
     clientTele.println("Content-Type: multipart/form-data; boundary=cam");
     clientTele.println();
     clientTele.print(head);
-  
-    uint8_t *fbBuf = fb->buf;
+
+    uint8_t* fbBuf = fb->buf;
     size_t fbLen = fb->len;
     for (size_t n = 0; n < fbLen; n = n + 1024) {
       if ((n + 1024) < fbLen) {
         clientTele.write(fbBuf, 1024);
         fbBuf += 1024;
-      }
-      else if (fbLen%1024>0) {
-        size_t remainder = fbLen%1024;
+      } else if (fbLen % 1024 > 0) {
+        size_t remainder = fbLen % 1024;
         clientTele.write(fbBuf, remainder);
       }
-    }  
-    
+    }
+
     clientTele.print(tail);
-    
+
     esp_camera_fb_return(fb);
-    
-    int waitTime = 100000;   // timeout 100 seconds
+
+    int waitTime = 100000;  // timeout 100 seconds
     long startTimer = millis();
     boolean state = false;
-    
-    while ((startTimer + waitTime) > millis()){
+
+    while ((startTimer + waitTime) > millis()) {
       Serial.print(".");
-      delay(1000);      
+      delay(1000);
       while (clientTele.available()) {
         char c = clientTele.read();
-        if (state==true) getBody += String(c);        
+        if (state == true) getBody += String(c);
         if (c == '\n') {
-          if (getAll.length()==0) state=true; 
+          if (getAll.length() == 0) state = true;
           getAll = "";
-        } 
-        else if (c != '\r')
+        } else if (c != '\r')
           getAll += String(c);
         startTimer = millis();
       }
-      if (getBody.length()>0) break;
+      if (getBody.length() > 0) break;
     }
     clientTele.stop();
     Serial.println(getBody);
-  }
-  else {
-    getBody="Connected to api.telegram.org failed.";
+  } else {
+    getBody = "Connected to api.telegram.org failed.";
     Serial.println("Connected to api.telegram.org failed.");
   }
   return;
